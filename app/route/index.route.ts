44d423097f3544c6f76f1a489b3c1ad0af40f4fe
fatihturker <fatihturker35@gmail.com>
2022-03-11 18:24:52 +0300

    Upgrade Common

--- app/route/index.route.ts
+++ app/route/index.route.ts
@@ -10,15 +10,8 @@ import {
 import { NextFunction, Request, Response } from 'express';
 import { Environment } from '../../environment';
 import { SmsQueueConsumer } from '../consumer/sms-queue.consumer';
-import {
-  publicRoutes as smsPublicRoutes,
-  adminRoutes as smsAdminRoutes,
-  router as smsRouter,
-} from './sms.route';
-import {
-  publicRoutes as monitorPublicRoutes,
-  router as monitorRouter,
-} from './monitor.route';
+import { router as smsRouter } from './sms.route';
+import { router as monitorRouter } from './monitor.route';
 
 const subRoutes = {
   root: '/',
@@ -32,22 +25,11 @@ export namespace Routes {
   var message_queue_provider: MessageQueueProvider;
   let errorHandlerUtil: ErrorHandlerUtil;
   const debugLogUtil = new DebugLogUtil();
-  var publicRoutes: string[] = [];
-  var adminRoutes: string[] = [];
-
-  function populateRoutes(mainRoute: string, routes: Array<string>) {
-    var populated = Array<string>();
-    for (const s of routes) {
-      populated.push(mainRoute + (s === '/' ? '' : s));
-    }
-
-    return populated;
-  }
 
   export const mount = (app: any) => {
     const preloadUtil = new PreloadUtil();
     environment = new Environment();
-    errorHandlerUtil = new ErrorHandlerUtil( debugLogUtil, environment.args() );
+    errorHandlerUtil = new ErrorHandlerUtil(debugLogUtil, environment.args());
     mongodb_provider = new MongoDbProvider(environment.args());
 
     message_queue_provider = new MessageQueueProvider(environment.args());
@@ -67,15 +49,6 @@ export namespace Routes {
     preloadUtil
       .preload(mongodb_provider)
       .then(() => console.log('DB preloads are completed.'));
-
-    publicRoutes = [
-      ...populateRoutes(subRoutes.monitor, monitorPublicRoutes),
-      ...populateRoutes(subRoutes.sms, smsPublicRoutes),
-    ];
-    console.log('Public Routes: ', publicRoutes);
-
-    adminRoutes = [...populateRoutes(subRoutes.sms, smsAdminRoutes)];
-    console.log('Admin Routes: ', adminRoutes);
 
     const responseInterceptor = (
       req: Request,
@@ -105,8 +78,6 @@ export namespace Routes {
         res.locals.ctx = await context(
           req,
           environment.args(),
-          publicRoutes,
-          adminRoutes,
           mongodb_provider,
           undefined,
           message_queue_provider
