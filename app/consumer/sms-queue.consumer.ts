ce8232783a3ec62e465fa84a18ddd67baee17804
Fatih Turker <fatihturker35@gmail.com>
2023-01-19 10:32:09 +0300

    Integrate DLQ

--- app/consumer/sms-queue.consumer.ts
+++ app/consumer/sms-queue.consumer.ts
@@ -1,9 +1,15 @@
-import { ContextArgs, MongoDbProvider, QueueConsumer, SmsActionType, } from '@open-template-hub/common';
+import {
+  ContextArgs,
+  MessageQueueChannelType,
+  MongoDbProvider,
+  QueueConsumer,
+  QueueMessage,
+  SmsActionType,
+} from '@open-template-hub/common';
 import { SmsController } from '../controller/sms.controller';
 import { Sms } from '../interface/sms.interface';
 
 export class SmsQueueConsumer implements QueueConsumer {
-
   private channel: any;
   private ctxArgs: ContextArgs = {} as ContextArgs;
   private smsController: SmsController;
@@ -12,16 +18,16 @@ export class SmsQueueConsumer implements QueueConsumer {
     this.smsController = new SmsController();
   }
 
-  init = ( channel: string, ctxArgs: ContextArgs ) => {
+  init = (channel: string, ctxArgs: ContextArgs) => {
     this.channel = channel;
     this.ctxArgs = ctxArgs;
     return this;
   };
 
-  onMessage = async ( msg: any ) => {
-    if ( msg !== null ) {
+  onMessage = async (msg: any) => {
+    if (msg !== null) {
       const msgStr = msg.content.toString();
-      const msgObj = JSON.parse( msgStr );
+      const msgObj = JSON.parse(msgStr);
 
       const message: SmsActionType = msgObj.message;
 
@@ -32,60 +38,86 @@ export class SmsQueueConsumer implements QueueConsumer {
       let phoneNumber: string | undefined;
       let params: any | undefined;
 
-      if ( message?.smsType && Object.keys( message.smsType )?.length > 0 ) {
-        messageKey = Object.keys( message.smsType )[ 0 ];
-        phoneNumber = ( message.smsType as any )[ messageKey ].params.phoneNumber;
-        params = ( message.smsType as any )[ messageKey ].params;
+      if (message?.smsType && Object.keys(message.smsType)?.length > 0) {
+        messageKey = Object.keys(message.smsType)[0];
+        phoneNumber = (message.smsType as any)[messageKey].params.phoneNumber;
+        params = (message.smsType as any)[messageKey].params;
       } else {
-        console.log( 'Message will be rejected: ', msgObj );
-        this.channel.reject( msg, false );
+        console.log('Message will be rejected: ', msgObj);
+        this.channel.reject(msg, false);
         return;
       }
 
-      if ( messageKey && phoneNumber && params ) {
+      if (messageKey && phoneNumber && params) {
         let hook = async () => {
           await this.smsController.sendSms(
-              this.ctxArgs.mongodb_provider as MongoDbProvider,
-              {
-                messageKey,
-                providerKey: 'Twilio',
-                to: phoneNumber,
-                payload: params,
-                languageCode: message.language,
-              } as Sms
+            this.ctxArgs.mongodb_provider as MongoDbProvider,
+            {
+              messageKey,
+              providerKey: 'Twilio',
+              to: phoneNumber,
+              payload: params,
+              languageCode: message.language,
+            } as Sms
           );
         };
 
-        await this.operate( msg, msgObj, requeue, hook );
+        await this.operate(msg, msgObj, requeue, hook);
       }
     }
   };
 
   private operate = async (
-      msg: any,
-      msgObj: any,
-      requeue: boolean,
-      hook: Function
+    msg: any,
+    msgObj: any,
+    requeue: boolean,
+    hook: Function
   ) => {
     try {
       console.log(
-          'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
       await hook();
-      await this.channel.ack( msg );
+      await this.channel.ack(msg);
       console.log(
-          'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
-    } catch ( e ) {
+    } catch (e) {
       console.log(
-          'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj,
-          e
+        'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj,
+        e
       );
 
-      this.channel.nack( msg, false, requeue );
+      this.channel.nack(msg, false, requeue);
+    }
+  };
+
+  private moveToDLQ = async (msg: any, requeue: boolean) => {
+    try {
+      const orchestrationChannelTag =
+        this.ctxArgs.envArgs.mqArgs?.orchestrationServerMessageQueueChannel;
+
+      const message = {
+        sender: MessageQueueChannelType.SMS,
+        receiver: MessageQueueChannelType.DLQ,
+        message: {
+          owner: MessageQueueChannelType.SMS,
+          msg,
+        },
+      } as QueueMessage;
+
+      await this.ctxArgs.message_queue_provider?.publish(
+        message,
+        orchestrationChannelTag as string
+      );
+
+      this.channel.reject(msg, false);
+    } catch (e) {
+      console.log('Error while moving message to DLQ: ', msg);
+      this.channel.nack(msg, false, requeue);
     }
   };
 }
